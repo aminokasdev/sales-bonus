81c24cc9cc93f383c547d623c85a101b4791a488
aminokasdev <sagyntayamina@gmail.com>
2026-02-26 17:17:54 +0500

    изменила на публичный

--- src/main.js
+++ src/main.js
@@ -94,7 +94,6 @@ function analyzeSalesData(data, options) {
             if (!seller.products_sold[item.sku]) {
                 seller.products_sold[item.sku] = 0;
             }
-
             seller.products_sold[item.sku] += item.quantity;
         });
     });
